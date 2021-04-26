cc41359aab1d6a887b1cbc0fdcea79f9e0b33752
adriansocho <79548856+adriansocho@users.noreply.github.com>
2021-04-26 21:31:55 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,7 +17,7 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service'
   }
-  service {'minectraft':
+  service {'minecraft':
     ensure => running,
     enable => true,
   }
